08d88164fa52134a9cdb46931c148f73fcb9c5bf
simarjot <simarjot0032@gmail.com>
2024-03-08 20:52:06 +0530

    added the folders

--- codeinblogs2.0/src/App.js
+++ codeinblogs2.0/src/App.js
@@ -3,27 +3,7 @@ import "./App.css";
 import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <button class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
-        ...
-      </button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
